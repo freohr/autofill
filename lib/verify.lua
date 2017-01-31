acdf25b4ef926fb86b8b7659604da93a96de0788
Nexela <nexela@gmail.com>
2017-01-31 11:08:19 -0500

    Remove item verify (temporarily)

--- lib/verify.lua
+++ lib/verify.lua
@@ -15,17 +15,18 @@ function Verify.fill_sets(fill_sets)
 end
 
 function Verify.item_sets(item_sets)
-    local _valid_items = function (_, k)
-        if game.item_prototypes[k] then
-            return true
-        else
-            MOD.log("Removing "..k.." Not a valid item", 1)
-        end
-    end
-    local meta = getmetatable(item_sets)
-    local obj = table.filter(item_sets, _valid_items)
-    if meta then setmetatable(obj, meta) end
-    return obj
+    -- local _valid_items = function (_, k)
+    --     if game.item_prototypes[k] then
+    --         return true
+    --     else
+    --         MOD.log("Removing "..k.." Not a valid item", 1)
+    --     end
+    -- end
+    -- local meta = getmetatable(item_sets)
+    -- local obj = table.filter(item_sets, _valid_items)
+    -- if meta then setmetatable(obj, meta) end
+    -- return obj
+    return item_sets
 end
 
 
